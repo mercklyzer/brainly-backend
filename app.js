1ebe2f834b31d91f7cad9e017f1d0affd4be21a3
mercklyzer <mercklyzer@gmail.com>
2022-02-01 20:23:33 +0800

    migrate www to app.js

--- app.js
+++ app.js
@@ -5,57 +5,126 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 const passport = require('passport');
+const socket = require('./socket')();
+var debug = require('debug')('brainly-server:server');
+var http = require('http');
 
-module.exports = (socket) => {
+// ROUTERS
+var signupRouter = require('./routes/signup');
+var loginRouter = require('./routes/login');
+var usersRouter = require('./routes/users');
+var questionsRouter = require('./routes/questions')(socket)
+var subjectsRouter = require('./routes/subjects')
+var threadsRouter = require('./routes/threads')(socket)
+var imagesRouter = require('./routes/images')
 
-  // ROUTERS
-  var signupRouter = require('./routes/signup');
-  var loginRouter = require('./routes/login');
-  var usersRouter = require('./routes/users');
-  var questionsRouter = require('./routes/questions')(socket)
-  var subjectsRouter = require('./routes/subjects')
-  var threadsRouter = require('./routes/threads')(socket)
-  var imagesRouter = require('./routes/images')
+var app = express();
 
-  var app = express();
+require('./auth/passport')(passport);
 
-  require('./auth/passport')(passport);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+app.use(cors());
 
-  app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-  app.use(logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+app.use('/signup', signupRouter);
+app.use('/login', loginRouter);
+app.use('/users', usersRouter);
+app.use('/questions', questionsRouter);
+app.use('/subjects', subjectsRouter);
+app.use('/threads', threadsRouter);
+app.use('/files', imagesRouter);
 
-  app.use('/signup', signupRouter);
-  app.use('/login', loginRouter);
-  app.use('/users', usersRouter);
-  app.use('/questions', questionsRouter);
-  app.use('/subjects', subjectsRouter);
-  app.use('/threads', threadsRouter);
-  app.use('/files', imagesRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    next(createError(404));
-  });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-  return app
+var server = http.createServer(app);
+
+socket.initialize(server);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+console.log(port);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
 }
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  console.log("listening on:",addr, addr.port);
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
