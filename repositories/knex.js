6cc5e155fd3f340660ec4a2289ef4c880877e849
mercklyzer <mercklyzer@gmail.com>
2022-04-23 13:22:38 +0800

    Update db credentials

--- repositories/knex.js
+++ repositories/knex.js
@@ -1,22 +1,12 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-        host : "brainly-db.c1ia2fko8aat.ap-southeast-1.rds.amazonaws.com",
-        user : "root",
-        password : "BRAINLY-passwordBautista",
+        host : "brainly.ctc2j4bind7f.ap-southeast-1.rds.amazonaws.com",
+        user : "admin",
+        password : "password",
         database : "brainly",
         port: 3306
     }
 });
-// const knex = require('knex')({
-//     client: 'mysql',
-//     connection: {
-//         host : "localhost",
-//         user : "root",
-//         password : "password",
-//         database : "brainly",
-//         port: 3306
-//     }
-// });
 
 module.exports = knex;
