929673a2cec62bab37a78146e8c5b5dc6715b2b9
mercklyzer <mercklyzer@gmail.com>
2022-05-23 21:04:18 +0800

    Fix thank bug attempt 1

--- app.js
+++ app.js
@@ -26,13 +26,8 @@ require('./auth/passport')(passport);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
