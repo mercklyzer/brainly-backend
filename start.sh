65116029214f5dcbefa706dbdc14f08bc87d1f96
mercklyzer <mercklyzer@gmail.com>
2023-08-06 22:53:26 +0800

    chore: update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # run create/update migration scripts
 echo "create-if-not-exist database"
-node ./node_modules/db-migrate/bin/db-migrate db:create brainly -e dev --config ./database/database.json
+node node db-migrate db:create brainly -e dev --config ./database/database.json
 echo "finished create-if-not-exist database"
 
 echo "update database"
