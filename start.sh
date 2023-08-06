b615b51f8ef3d9f0747207274cc1381aa600d65b
mercklyzer <mercklyzer@gmail.com>
2023-08-06 22:48:19 +0800

    chore: update start script

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
- 
+
 # run create/update migration scripts
 echo "create-if-not-exist database"
 node ./node_modules/db-migrate/bin/db-migrate db:create brainly -e dev --config ./database/database.json
 echo "finished create-if-not-exist database"
- 
+
 echo "update database"
 node ./node_modules/db-migrate/bin/db-migrate up -e dev --config ./database/database.json -m ./database/migrations
 echo "finished update database"
- 
+
 # run node app
 echo "running node app"
-./node_modules/.bin/nodemon app.js
+# ./node_modules/.bin/nodemon app.js
+node app.js
